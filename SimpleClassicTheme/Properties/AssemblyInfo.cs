5a9b8fb841f9288cef79b9964a2be28242fbfe10
Anis Errais <anis1errais@live.com>
2020-08-03 22:14:20 +0200

    .

--- SimpleClassicTheme/Properties/AssemblyInfo.cs
+++ SimpleClassicTheme/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Simple Classic Theme")]
-[assembly: AssemblyCopyright("© 1337ftw 2019")]
+[assembly: AssemblyCopyright("© 1337ftw 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
