1de22c791ed50eef85d04fd68d84bf468601026d
AEAEAEAE4343 <34800109+AEAEAEAE4343@users.noreply.github.com>
2021-10-27 21:57:07 +0200

    Bumped down version to 1.4.9 Changed update form to use system font

--- SimpleClassicTheme/Forms/UpdateForm.Designer.cs
+++ SimpleClassicTheme/Forms/UpdateForm.Designer.cs
@@ -57,10 +57,10 @@ namespace SimpleClassicTheme
 			// pictureBox1
 			// 
 			this.pictureBox1.Image = global::SimpleClassicTheme.Properties.Resources.sct_light_275;
-			this.pictureBox1.Location = new System.Drawing.Point(22, 23);
-			this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
+			this.pictureBox1.Location = new System.Drawing.Point(22, 22);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(275, 275);
+			this.pictureBox1.Size = new System.Drawing.Size(264, 264);
 			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
 			this.pictureBox1.TabIndex = 0;
 			this.pictureBox1.TabStop = false;
@@ -68,31 +68,32 @@ namespace SimpleClassicTheme
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(17, 304);
+			this.label1.Location = new System.Drawing.Point(17, 292);
 			this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(209, 25);
+			this.label1.Size = new System.Drawing.Size(203, 24);
 			this.label1.TabIndex = 1;
 			this.label1.Text = "Checking for updates...";
 			// 
 			// progressBar1
 			// 
-			this.progressBar1.Location = new System.Drawing.Point(22, 332);
+			this.progressBar1.Location = new System.Drawing.Point(22, 319);
+			this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
 			this.progressBar1.Name = "progressBar1";
-			this.progressBar1.Size = new System.Drawing.Size(275, 23);
+			this.progressBar1.Size = new System.Drawing.Size(264, 22);
 			this.progressBar1.TabIndex = 2;
 			// 
 			// Updater
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(317, 371);
+			this.ClientSize = new System.Drawing.Size(310, 356);
 			this.Controls.Add(this.progressBar1);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.pictureBox1);
-			this.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Margin = new System.Windows.Forms.Padding(6);
+			this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.Name = "Updater";
 			this.Text = "SCT - update checker";
 			this.Load += new System.EventHandler(this.Updater_Load_1);
